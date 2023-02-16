29aaeedb0349de48bf975712037651c07850b810
Hoppy <mbraets@student.42.fr>
2023-02-16 11:37:32 +0100

    Mariadb finish entrypoint

--- srcs/requirements/mariadb/conf/entrypoint.sh
+++ srcs/requirements/mariadb/conf/entrypoint.sh
@@ -1,12 +1,19 @@
-mariadbd -uroot &
+
+mkdir -p ${VOLUME_WP_PATH}
+mkdir -p ${VOLUME_DB_PATH}
+
+echo "--------------- Mysql Install ---------------"
+mysql_install_db -uroot --basedir=/usr --datadir=/var/lib/mysql
+mariadbd -uroot & sleep 2
+echo "--------------- Inject SQL ---------------"
 mysql << EOF
-CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_USER_PASSWORD}';
-GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'%';
+CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_USER_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_USER_PASSWORD}';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 FLUSH PRIVILEGES;
 EOF
-# DELETE FROM mysql.user WHERE user=''
-# DELETE FROM mysql.user WHERE user='root'
-# FLUSH PRIVILEGES;
+echo "--------------- Kill Mysql ---------------"
 killall mariadbd
-exec "$@"
+echo "--------------- Starting Mysql ---------------"
+mariadbd -uroot
