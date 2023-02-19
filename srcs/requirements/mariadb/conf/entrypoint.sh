6fed1fddb02d9d2f6adb50ede52698b0e30bf626
Hoppy <mbraets@student.42.fr>
2023-02-19 21:42:05 +0100

    Add color to log

--- srcs/requirements/mariadb/conf/entrypoint.sh
+++ srcs/requirements/mariadb/conf/entrypoint.sh
@@ -2,10 +2,10 @@
 mkdir -p ${VOLUME_WP_PATH}
 mkdir -p ${VOLUME_DB_PATH}
 
-echo "--------------- Mysql Install ---------------"
+echo -e "\033[32;1m--------------- Mysql Install ---------------\033[0m"
 mysql_install_db -uroot --basedir=/usr --datadir=/var/lib/mysql
 mariadbd -uroot & sleep 2
-echo "--------------- Inject SQL ---------------"
+echo -e "\033[32;1m--------------- Inject SQL ---------------\033[0m"
 mysql << EOF
 CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_USER_PASSWORD}';
@@ -13,7 +13,7 @@ GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'localhost' I
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 FLUSH PRIVILEGES;
 EOF
-echo "--------------- Kill Mysql ---------------"
+echo -e "\033[32;1m--------------- Kill Mysql ---------------\033[0m"
 killall mariadbd
-echo "--------------- Starting Mysql ---------------"
+echo -e "\033[32;1m--------------- Starting Mysql ---------------\033[0m"
 mariadbd -uroot
