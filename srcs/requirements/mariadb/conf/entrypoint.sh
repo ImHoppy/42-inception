2011837f888b34ed89c715bfe965b13461b9f56b
Hoppy <mbraets@student.42.fr>
2023-02-22 13:47:10 +0100

    Install mariadb only when not exist

--- srcs/requirements/mariadb/conf/entrypoint.sh
+++ srcs/requirements/mariadb/conf/entrypoint.sh
@@ -2,18 +2,23 @@
 mkdir -p ${VOLUME_WP_PATH}
 mkdir -p ${VOLUME_DB_PATH}
 
-echo -e "\033[32;1m--------------- Mysql Install ---------------\033[0m"
-mysql_install_db -uroot --basedir=/usr --datadir=/var/lib/mysql
-mariadbd -uroot & sleep 2
-echo -e "\033[32;1m--------------- Inject SQL ---------------\033[0m"
-mysql << EOF
+if [ ! -d /var/lib/mysql/${MYSQL_DATABASE} ]; then
+	echo -e "\033[32;1m--------------- Mysql Install ---------------\033[0m"
+	mysql_install_db -uroot --basedir=/usr --datadir=/var/lib/mysql
+	mariadbd -uroot & sleep 2
+	echo -e "\033[32;1m--------------- Inject SQL ---------------\033[0m"
+	until mysqladmin ping; do
+		sleep 2
+	done
+	mysql << EOF
 CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_USER_PASSWORD}';
 GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_USER_PASSWORD}';
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 FLUSH PRIVILEGES;
 EOF
-echo -e "\033[32;1m--------------- Kill Mysql ---------------\033[0m"
-killall mariadbd
+	echo -e "\033[32;1m--------------- Kill Mysql ---------------\033[0m"
+	killall mariadbd
+fi
 echo -e "\033[32;1m--------------- Starting Mysql ---------------\033[0m"
 mariadbd -uroot
