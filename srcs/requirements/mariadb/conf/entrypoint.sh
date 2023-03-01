d01522a0609076cb8abc61337aae01b9ea960a2a
Hoppy <mbraets@student.42.fr>
2023-03-01 19:23:39 +0100

    Format / Fix sh error

--- srcs/requirements/mariadb/conf/entrypoint.sh
+++ srcs/requirements/mariadb/conf/entrypoint.sh
@@ -1,24 +1,25 @@
+#!/bin/sh
 
-mkdir -p ${VOLUME_WP_PATH}
-mkdir -p ${VOLUME_DB_PATH}
+mkdir -p "$VOLUME_WP_PATH"
+mkdir -p "$VOLUME_DB_PATH"
 
-if [ ! -d /var/lib/mysql/${MYSQL_DATABASE} ]; then
-	echo -e "\033[32;1m--------------- Mysql Install ---------------\033[0m"
+if [ ! -d /var/lib/mysql/"$MYSQL_DATABASE" ]; then
+	printf "\033[32;1m--------------- Mysql Install ---------------\033[0m"
 	mysql_install_db -uroot --basedir=/usr --datadir=/var/lib/mysql
 	mariadbd -uroot & sleep 2
-	echo -e "\033[32;1m--------------- Inject SQL ---------------\033[0m"
+	printf "\033[32;1m--------------- Inject SQL ---------------\033[0m"
 	until mysqladmin ping; do
 		sleep 2
 	done
 	mysql <<- EOF
-CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
-CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-FLUSH PRIVILEGES;
-EOF
-	echo -e "\033[32;1m--------------- Kill Mysql ---------------\033[0m"
+		CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+		CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+		GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+		ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+		FLUSH PRIVILEGES;
+	EOF
+	printf "\033[32;1m--------------- Kill Mysql ---------------\033[0m"
 	killall mariadbd
 fi
-echo -e "\033[32;1m--------------- Starting Mysql ---------------\033[0m"
+printf "\033[32;1m--------------- Starting Mysql ---------------\033[0m"
 mariadbd -uroot
--- srcs/requirements/wordpress/conf/entrypoint.sh
+++ srcs/requirements/wordpress/conf/entrypoint.sh
@@ -1,38 +1,41 @@
-echo -e "\033[32;1m--------------- Waiting for mysql ---------------\033[0m"
-until mariadb-admin --host=${WORDPRESS_DB_HOST} --user=${WORDPRESS_DB_USER} --password=${WORDPRESS_DB_PASSWORD} --silent ping; do
-	echo -e "\033[32;1m--------------- Mysql is down ---------------\033[0m"
+#!/bin/sh
+
+printf "\033[32;1m--------------- Waiting for mysql ---------------\033[0m"
+until mariadb-admin --host="$WORDPRESS_DB_HOST" --user="$WORDPRESS_DB_USER" --password="$WORDPRESS_DB_PASSWORD" --silent ping; do
+	printf "\033[32;1m--------------- Mysql is down ---------------\033[0m"
 	sleep 4
 done
 if [ ! -f "/var/www/html/wp-config.php" ]; then
-	echo -e "\033[32;1m--------------- Installing wordpress ---------------\033[0m"
+	printf "\033[32;1m--------------- Installing wordpress ---------------\033[0m"
 	wp core download --path=/var/www/html
 
 	wp config create \
 		--path=/var/www/html \
-		--dbname=$WORDPRESS_DB_NAME \
-		--dbuser=$WORDPRESS_DB_USER \
-		--dbpass=$WORDPRESS_DB_PASSWORD \
-		--dbhost=$WORDPRESS_DB_HOST \
-		--dbprefix=$WORDPRESS_TABLE_PREFIX
+		--dbname="$WORDPRESS_DB_NAME" \
+		--dbuser="$WORDPRESS_DB_USER" \
+		--dbpass="$WORDPRESS_DB_PASSWORD" \
+		--dbhost="$WORDPRESS_DB_HOST" \
+		--dbprefix="$WORDPRESS_TABLE_PREFIX"
 
 	wp core install \
 		--path=/var/www/html \
-		--url=$DOMAIN_NAME \
-		--title=$WORDPRESS_TITLE \
-		--admin_user=$WORDPRESS_ADMIN_USER \
-		--admin_password=$WORDPRESS_ADMIN_PASSWORD \
-		--admin_email=$WORDPRESS_ADMIN_EMAIL
+		--url="$DOMAIN_NAME" \
+		--title="$WORDPRESS_TITLE" \
+		--admin_user="$WORDPRESS_ADMIN_USER" \
+		--admin_password="$WORDPRESS_ADMIN_PASSWORD" \
+		--admin_email="$WORDPRESS_ADMIN_EMAIL" \
+		--skip-email
 
 	wp user create \
-		$WORDPRESS_USER \
-		$WORDPRESS_USER_EMAIL \
+		"$WORDPRESS_USER" \
+		"$WORDPRESS_USER_EMAIL" \
 		--role=author \
-		--user_pass=$WORDPRESS_USER_PASSWORD \
+		--user_pass="$WORDPRESS_USER_PASSWORD" \
 		--path=/var/www/html
 
-	echo -e "\033[32;1m--------------- Wordpress installed ---------------\033[0m"
+	printf "\033[32;1m--------------- Wordpress installed ---------------\033[0m"
 else
-	echo -e "\033[32;1m--------------- Wordpress already installed ---------------\033[0m"
+	printf "\033[32;1m--------------- Wordpress already installed ---------------\033[0m"
 fi
-echo -e "\033[32;1m--------------- Starting php-fpm ---------------\033[0m"
+printf "\033[32;1m--------------- Starting php-fpm ---------------\033[0m"
 exec "$@"
